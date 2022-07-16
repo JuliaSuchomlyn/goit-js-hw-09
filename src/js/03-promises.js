8caaea54233aaf24025ca439d731d4cb2efce227
Julia <juliasuchomlyn@gmail.com>
2022-07-16 19:49:07 +0300

    add task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+        return resolve({position, delay})
     // Fulfill
-  } else {
+    } else {
+      return reject({ position, delay });
     // Reject
   }
+  })
 }
+
+function onButtonSubmit(e) {
+  e.preventDefault();
+
+  let delay = e.currentTarget.delay.value;
+  const step = e.currentTarget.step.value;
+  const amount = e.currentTarget.amount.value;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => { Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); })
+      .catch(({ position, delay }) => { Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`); });
+    delay += step;
+  }
+  
+}
+formEl.addEventListener('click', onButtonSubmit);
